6394fc16014f1803ba09bf0906185240cdf98f97
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-01-24 19:50:19 +0100

    Added ability to review error jsons

--- validator.py
+++ validator.py
@@ -4,11 +4,17 @@ import datetime
 import csv
 time = datetime.datetime.now()
 category = os.path.basename(os.getcwd())
+#Create backup
 csvFilename = rf'{category} errorList.csv'
 csvBackupname = time.strftime('%Y-%m-%d--%H-%M-%S')+rf' {csvFilename}'+r'.backup'
 if os.path.exists(csvFilename):
 	os.rename(csvFilename,csvBackupname)
 csvFile = open(csvFilename, 'w')
+#Create folder for inspection
+reviewFolder = r'./manualReview'
+if not os.path.exists(reviewFolder):
+	os.mkdir(reviewFolder)
+reviewCount = 0
 fileCount = 0
 errorCount = 0
 for root, dirs, files in os.walk('.'):
@@ -18,24 +24,34 @@ for root, dirs, files in os.walk('.'):
 			idFromFilename = file.split('.')[0]
 			try:
 				data = json.load(open(os.path.join(file), 'r'))
+			#Wenn ein Fehler beim Zugriff auf die Daten passiert wird das File ins ReviewFolder verschoben
 			except:
-				print(rf"Fehler bei {idFromFilename}")
-				os.remove(idFromFilename+'.json')
+				print(f'So siehts file aus: {file}')
+				print(rf"Fehler bei ID: {idFromFilename} - in reviewFolder verschoben")
+				os.rename(rf'{idFromFilename}.json', rf'{reviewFolder}/No Access to Data - {idFromFilename}.json')
+				reviewCount += 1
+				data = []
+				continue
 			#Check ob ein Profil gescraped wurde, welches keine ID besitzt
 			if 'id' in data:
 				idFromData = str(data['id'])
 				if idFromData != idFromFilename:
+					print(rf'Datenid: {idFromData} stimmt nicht mit filenameID {idFromFilename} zusammen')
 					errorCount += 1
 					csvFile.write(f'{idFromFilename}\n')
 			#Alle Profile ohne ID sehen so aus: https://imgur.com/a/2LJden3
 			else:
-				print(rf'Datei {idFromFilename}.json hat keine ID')
+				print(rf'Datei {idFromFilename}.json hat keine ID - in reviewFolder verschoben')
 				fileCount -= 1
-				#Das fehlerhafte Profil wird geloescht. Es wird nicht in die .csv geschrieben, da es nicht nochmal gescraped werden soll
-				os.remove(idFromFilename+'.json')
+				#Das fehlerhafte Profil wird ins ReviewFolder verschoben. Es wird nicht in die .csv geschrieben, da es nicht nochmal gescraped werden soll
+				os.rename(rf'{idFromFilename}.json', rf'{reviewFolder}/No ID in Data - {idFromFilename}.json')
+				reviewCount += 1
+	break
 csvFile.close()
 errorRate = round((errorCount/fileCount)*100, 2)
 print(f'{errorCount} of {fileCount} files were faulty ({errorRate}%)')
+if reviewCount > 0:
+	print(rf'{reviewCount} file(s) moved to reviewFolder - please manually review these files.')
 log = open(rf'{category}_log.txt', 'a')
 dateStr = time.strftime('%d.%m.%Y')
 timeStr = time.strftime('%H:%M:%S')
